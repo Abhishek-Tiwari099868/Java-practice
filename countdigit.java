3f9299cb5ea0f6af86b6979be77399cbba136e43
Abhishek-Tiwari099868 <at0263690@gmail.com>
2026-03-30 09:45:37 +0530

    Fix variable declaration and sum calculation

--- countdigit.java
+++ countdigit.java
@@ -6,10 +6,14 @@ public class countdigit {
         System.out.println("enter the number:");
         n = sc.nextInt();
         int count = 0;
+        it sum = 0;
         while(n!=0){
+            int lastdigit = n%10;
+            sum = sum + lastdigit;
             n = n/10;
             count++;
         }
         System.out.println("number of digits in the given number is :" + count);
+        System.out.println("sum of digits in the given number is :" + sum);
     }
 }
